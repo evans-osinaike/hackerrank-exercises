d304b3dd8cd04e385ef61b8a270e2285743f63f6
Osinaike Evans <the.evans.osinaike@gmail.com>
2021-07-03 22:04:50 +0100

    solved the even-odd problem

---
+++ 30 Days of Code/Day 3-Intro to Conditional Statements.py
@@ -0,0 +1,26 @@
+#!/bin/python3
+
+# Given an integer, n, perform the following conditional actions:
+
+# If n is odd, print Weird
+# If n is even and in the inclusive range of 2 to 5, print Not Weird
+# If n is even and in the inclusive range of 6 to 20, print Weird
+# If n is even and greater than 20, print Not Weird
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    N = int(input().strip())
+
+    if N%2 != 0:
+        print('Weird')
+    elif (2 <= N <= 5) or N > 20:
+        print('Not Weird')
+    elif 6 <= N <= 20:
+        print('Weird')
